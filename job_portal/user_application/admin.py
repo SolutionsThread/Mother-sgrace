b7071326f1e3067c087b6847edfecf2ad0de352e
Souradip Ganguly <operation@solutionsthread.com>
2020-07-28 17:25:33 -0400

    first commit

---
+++ job_portal/user_application/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from user_application.models import UserProfileInfo, User
+
+admin.site.register(UserProfileInfo)
+
+
+
+
---
+++ job_portal/user_application/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UserApplicationConfig(AppConfig):
+    name = 'user_application'
